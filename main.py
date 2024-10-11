b9bc922174ecf16880cc56a39093eee406b5ed5c
Trent Spoolstra <tspoolst@gmail.com>
2024-10-10 22:12:40 -0600

    extracted string to html file and added sample build test

--- main.py
+++ main.py
@@ -7,21 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_root():
-    return '''<body>
-<big><big><big>
-<h1>The Draught of Living Death</h1>
-<ol>
-  <li>Add the Infusion of Wormwood.<br><img src="/static/InfusionofWormwood.webp" width="250" height="136"></li>
-  <li>Add the Powdered Root of Asphodel.<br><img src="/static/Powdered_Asphodel.webp" width="250" height="136"></li>
-  <li>Stir twice clockwise.</li>
-  <li>Add the sloth brain.<br><img src="/static/A_Sloth_brain.webp" width="250" height="136"></li>
-  <li>Add the Sopophorous Bean's juice.<br><img src="/static/Sopophorous_Bean_HM.webp" width="250" height="136"></li>
-  <li>Stir seven times anti-clockwise.</li>
-</ol>
-</big></big></big>
-</body>'''
+  with open('index.html', 'r') as file:
+    return file.read()
 
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
 
